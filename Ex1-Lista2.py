e738ed1a56de946e9342cf5355926a0b8df340c4
Jeansds <54490681+Jeansds@users.noreply.github.com>
2019-10-14 14:32:25 -0300

    Add files via upload

---
+++ Ex1-Lista2.py
@@ -0,0 +1,17 @@
+# Faça um Programa que peça dois números e imprima o maior deles. 
+
+Numero_1=float(input("digite o primeiro numero")) 
+
+Numero_2=float(input("digite o segundo numero")) 
+
+if Numero_1>Numero_2: 
+
+    print(Numero_1) 
+
+elif Numero_2>Numero_1: 
+
+    print(Numero_2) 
+
+else: 
+
+    print("ambos são iguais") 
